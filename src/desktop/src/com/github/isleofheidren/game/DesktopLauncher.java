48a194b9f962a670ea0426d19992c7838c2c2fd5
20Heedje <raber.9@wright.edu>
2024-02-13 17:16:18 -0500

    added .DS_Store to ignore

--- src/desktop/src/com/github/isleofheidren/game/DesktopLauncher.java
+++ src/desktop/src/com/github/isleofheidren/game/DesktopLauncher.java
@@ -3,6 +3,10 @@ package com.github.isleofheidren.game;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+import com.github.isleofheidren.game.game;
+>>>>>>> 4a1c676 (imported project framework)
 =======
 import com.github.isleofheidren.game.game;
 >>>>>>> 4a1c676 (imported project framework)
@@ -13,9 +17,14 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 <<<<<<< HEAD
+<<<<<<< HEAD
 		config.setWindowedMode(1000, 600);
 		config.setTitle("IsleOfHeidren");
 		new Lwjgl3Application(new Heidren(), config);
+=======
+		config.setTitle("IsleOfHeidren");
+		new Lwjgl3Application(new game(), config);
+>>>>>>> 4a1c676 (imported project framework)
 =======
 		config.setTitle("IsleOfHeidren");
 		new Lwjgl3Application(new game(), config);
