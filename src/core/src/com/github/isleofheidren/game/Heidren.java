1f315958a39e3ed38ca294c14e0c129a9aaa28f9
Renee <reneepaxson@gmail.com>
2024-03-05 01:03:17 -0500

    messing w skins, renamed  class to  to avoid confusion (theoretically)

---
+++ src/core/src/com/github/isleofheidren/game/Heidren.java
@@ -0,0 +1,40 @@
+// "driver"
+package com.github.isleofheidren.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Heidren extends Game {
+	public SpriteBatch batch;
+	public BitmapFont font;
+
+
+
+	public void create () {
+
+
+		batch = new SpriteBatch();
+
+		font = new BitmapFont();
+
+		this.setScreen(new MainMenuScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		font.dispose();
+	}
+}
--- src/desktop/src/com/github/isleofheidren/game/DesktopLauncher.java
+++ src/desktop/src/com/github/isleofheidren/game/DesktopLauncher.java
@@ -2,7 +2,6 @@ package com.github.isleofheidren.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.github.isleofheidren.game.game;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -11,6 +10,6 @@ public class DesktopLauncher {
 		config.setForegroundFPS(60);
 		config.setWindowedMode(1000, 600);
 		config.setTitle("IsleOfHeidren");
-		new Lwjgl3Application(new game(), config);
+		new Lwjgl3Application(new Heidren(), config);
 	}
 }
