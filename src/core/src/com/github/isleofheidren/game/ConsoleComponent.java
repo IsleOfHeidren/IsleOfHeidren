85f0f9c4bec516436c6767ef032d146332a5913e
Joseph Claborn <joseph.claborn2@gmail.com>
2024-03-12 16:37:23 -0400

    recreated console component again

---
+++ src/core/src/com/github/isleofheidren/game/ConsoleComponent.java
@@ -0,0 +1,44 @@
+package com.github.isleofheidren.game;
+
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
+import com.badlogic.gdx.scenes.scene2d.ui.TextAreaStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+public class ConsoleComponent extends Table {
+    private ScrollPane scrollPane;
+    private TextArea textArea;
+
+    public ConsoleComponent() {
+        textArea = new TextArea("", new TextArea.TextAreaStyle());
+        scrollPane = new ScrollPane(textArea);
+
+        add(scrollPane).expand().fill().row();
+    }
+
+    public void appendMessage(String message, MessageType type) {
+        String prefix = getMessagePrefix(type);
+        String formattedMessage = prefix + message + "\n";
+        textArea.appendText(formattedMessage);
+
+        // Set scroll position to maximum after each update
+        scrollPane.setScrollPercentY(100);
+    }
+
+    private String getMessagePrefix(MessageType type) {
+        switch (type) {
+            case NPC_DIALOG:
+                return "Character Name: ";
+            case GAME_EVENT:
+                return "Game Event: ";
+            case COMBAT_RESULT:
+                return "Combat Result: ";
+            case STORY_TEXT:
+                return "Story: ";
+            case SKILL_CHECK_RESULT:
+                return "Skill Check: ";
+            default:
+                return "";
+        }
+    }
+}
--- src/core/src/com/github/isleofheidren/game/Heidren.java
+++ src/core/src/com/github/isleofheidren/game/Heidren.java
@@ -1,34 +1,46 @@
 // "driver"
 package com.github.isleofheidren.game;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.github.isleofheidren.game.MessageType;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Heidren extends Game {
+    private Stage stage;
 	public SpriteBatch batch;
-	public BitmapFont font;
+    private ConsoleComponent console;
 
-
-
-	public void create () {
-
-
+    @Override
+    public void create() {
 		batch = new SpriteBatch();
+        stage = new Stage(new FitViewport(800, 600));
+        Gdx.input.setInputProcessor(stage);
 
-		font = new BitmapFont();
+        console = new ConsoleComponent();
+        stage.addActor(console);
 
-		this.setScreen(new MainMenuScreen(this));
-	}
+        // Example
+        console.appendMessage("Hello, world!", MessageType.GAME_EVENT);
+        console.appendMessage("This is NPC dialog.", MessageType.NPC_DIALOG);
+    }
 
-	@Override
-	public void render () {
-		super.render();
-	}
+    @Override
+    public void render() {
+        super.render();
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
 
-	@Override
-	public void dispose () {
-		batch.dispose();
-		font.dispose();
-	}
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
 }
