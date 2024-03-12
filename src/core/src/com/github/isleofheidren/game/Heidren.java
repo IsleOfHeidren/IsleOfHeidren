8dd1cce8392e04e0c65f9db118e1a16a18ef6681
Bella <67770613+bricklerb@users.noreply.github.com>
2024-03-12 19:07:02 -0400

    skin changes

--- src/core/src/com/github/isleofheidren/game/Heidren.java
+++ src/core/src/com/github/isleofheidren/game/Heidren.java
@@ -1,18 +1,20 @@
 // "driver"
 package com.github.isleofheidren.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class Heidren extends Game {
 	public SpriteBatch batch;
 	public BitmapFont font;
-
+	public static Skin skin;
 
 
 	public void create () {
-
+		skin = new Skin(Gdx.files.internal("skin/skin.json"));
 
 		batch = new SpriteBatch();
 
