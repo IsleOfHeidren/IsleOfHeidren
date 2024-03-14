3af20573c1ebbfcb32a698d452587560549664b2
Bella <67770613+bricklerb@users.noreply.github.com>
2024-03-14 01:41:20 -0400

    implemented repo for story events

---
+++ src/core/src/com/github/isleofheidren/game/repos/StoryEventRepo.java
@@ -0,0 +1,25 @@
+package com.github.isleofheidren.game.repos;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.github.isleofheidren.game.models.Event;
+import com.github.isleofheidren.game.models.StoryEvent;
+import com.google.gson.Gson;
+
+/*
+Repo for story game events
+ */
+public class StoryEventRepo implements JSONRepo<StoryEvent> {
+
+    /*
+    Gets a story event based on the ID
+     */
+    @Override
+    public StoryEvent get(String id) {
+        String filePath = "data/events/" + id;
+        FileHandle file = Gdx.files.internal(filePath);
+
+        Gson gson = new Gson();
+        return gson.fromJson(file.reader(), StoryEvent.class);
+    }
+}
