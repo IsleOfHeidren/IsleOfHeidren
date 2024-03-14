1a267257f9662ab56847259f51ba474c673cc6a2
Bella <67770613+bricklerb@users.noreply.github.com>
2024-03-14 02:08:27 -0400

    updated event to better represent object needs

--- src/core/src/com/github/isleofheidren/game/models/CombatEvent.java
+++ src/core/src/com/github/isleofheidren/game/models/CombatEvent.java
@@ -1,8 +1,34 @@
 package com.github.isleofheidren.game.models;
 
+/*
+Model for combat event, includes all properties necessary for combat to happen
+ */
 public class CombatEvent extends Event{
     private PlayerCharacter currentCharacter;
-    private String[] actionsText;
     private PlayerCharacter[] otherCharacters;
     private Monster[] monsters;
+
+    public PlayerCharacter getCurrentCharacter() {
+        return currentCharacter;
+    }
+
+    public void setCurrentCharacter(PlayerCharacter currentCharacter) {
+        this.currentCharacter = currentCharacter;
+    }
+
+    public PlayerCharacter[] getOtherCharacters() {
+        return otherCharacters;
+    }
+
+    public void setOtherCharacters(PlayerCharacter[] otherCharacters) {
+        this.otherCharacters = otherCharacters;
+    }
+
+    public Monster[] getMonsters() {
+        return monsters;
+    }
+
+    public void setMonsters(Monster[] monsters) {
+        this.monsters = monsters;
+    }
 }
