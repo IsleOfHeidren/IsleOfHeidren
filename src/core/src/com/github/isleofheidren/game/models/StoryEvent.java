0111913d9afee96a385c64c5b7862d34cbf6b3b8
Bella <67770613+bricklerb@users.noreply.github.com>
2024-03-14 01:41:06 -0400

    game models

--- src/core/src/com/github/isleofheidren/game/models/StoryEvent.java
+++ src/core/src/com/github/isleofheidren/game/models/StoryEvent.java
@@ -2,6 +2,39 @@ package com.github.isleofheidren.game.models;
 
 public class StoryEvent extends Event {
     private int[] branches;
-    private string[] branchesText;
-    private
+    private String[] branchesText;
+    private String consoleOutputText;
+
+    /**
+     *
+     */
+    public StoryEvent() {
+    }
+
+    public String getConsoleOutputText() {
+        return consoleOutputText;
+    }
+
+    public void setConsoleOutputText(String consoleOutputText) {
+        this.consoleOutputText = consoleOutputText;
+    }
+
+    public String[] getBranchesText() {
+        return branchesText;
+    }
+
+    public void setBranchesText(String[] branchesText) {
+        this.branchesText = branchesText;
+    }
+
+    public int[] getBranches() {
+        return branches;
+    }
+
+    public void setBranches(int[] branches) {
+        this.branches = branches;
+    }
+
+
+    //TODO add in a prop for the image URI or the image bits themselves
 }
