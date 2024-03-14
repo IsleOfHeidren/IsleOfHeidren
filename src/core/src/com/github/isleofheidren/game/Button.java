e1083541e6cd548b9f5599759ba3d79d9f9356b3
Renee <reneepaxson@gmail.com>
2024-03-14 01:47:06 -0400

    start screen -> main screen secured bitchessssz

--- src/core/src/com/github/isleofheidren/game/Button.java
+++ src/core/src/com/github/isleofheidren/game/Button.java
@@ -1,12 +1,15 @@
 package com.github.isleofheidren.game;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-//import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-public class Button{
+public class Button extends Heidren {
 
     Stage stage;
     TextButton button;
@@ -15,7 +18,7 @@ public class Button{
     //Skin skin;
     //TextureAtlas buttonAtlas;
 
-    @Override
+    //@Override
     public void create(String text, Skin skin) {
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
@@ -30,6 +33,11 @@ public class Button{
     }
 
     @Override
+    public void create() {
+
+    }
+
+    //@Override
     public void render() {
         super.render();
         stage.draw();
