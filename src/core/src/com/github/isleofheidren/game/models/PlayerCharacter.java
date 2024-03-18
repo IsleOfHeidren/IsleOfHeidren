88a63bc27b168f8389cd1f7ba0c1f53cc50f44df
Bella <67770613+bricklerb@users.noreply.github.com>
2024-03-18 16:53:20 -0400

    added health to PC

--- src/core/src/com/github/isleofheidren/game/models/PlayerCharacter.java
+++ src/core/src/com/github/isleofheidren/game/models/PlayerCharacter.java
@@ -4,6 +4,7 @@ public class PlayerCharacter {
     private String name;
     private Feature[] features;
     private int armorClass;
+    private int health;
 
     public PlayerCharacter() {
 
@@ -31,5 +32,13 @@ public class PlayerCharacter {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
     }
 }
