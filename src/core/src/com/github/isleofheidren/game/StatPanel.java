a92a1a1b5ffdfff36076e674720d4fd25704dcd2
Bella <67770613+bricklerb@users.noreply.github.com>
2024-03-18 16:50:44 -0400

    init panel class

---
+++ src/core/src/com/github/isleofheidren/game/StatPanel.java
@@ -0,0 +1,12 @@
+package com.github.isleofheidren.game;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+public class StatPanel extends Table {
+    public StatPanel() {
+        super();
+    }
+
+    public StatPanel(PlayerCharacter characters) {
+        super();
+    }
+}
