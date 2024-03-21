fb61337abdea6fef6861ba9f5f8f2a5f47c868aa
Renee <reneepaxson@gmail.com>
2024-03-21 03:24:52 -0400

    button panel edits

--- src/core/src/com/github/isleofheidren/game/MainMenuScreen.java
+++ src/core/src/com/github/isleofheidren/game/MainMenuScreen.java
@@ -64,64 +64,85 @@ public class MainMenuScreen implements Screen {
 
         buttonStage.addActor(buttonpanel); // may make lookup easier later
 
-        buttonpanel.setSize(100,200);
+        buttonpanel.setSize(150,200);
 
         // bg = new Texture(Gdx.files.internal("sprites/bg.png")); //probably junk
-
         // table.setSkin(Heidren.skin); // don't remember what this is for
 
         roottable.setDebug(true); // shows table parameters
 
-        // playing with simplified animation
+        // playing with simplified animation (broken)
         //runningAnimation = new Animation<TextureRegion>(0.125f, atlas.findRegions("monk"), Animation.PlayMode.LOOP);
 
         // test labels for blocking
         Label title = new Label("Isle of Heidren", Heidren.skin.optional("default", Label.LabelStyle.class));
         Label space = new Label("", Heidren.skin.optional("default", Label.LabelStyle.class));
 
-        console.appendMessage("Hello, world!", MessageType.GAME_EVENT); // Game Event Ex.
+        // testing button wrap
+        Label longbuttontext = new Label("this is a really long message, like reeeeally really long.", Heidren.skin.optional("default", Label.LabelStyle.class));
+        longbuttontext.setWrap(true);
+        longbuttontext.setWidth(buttonpanel.getWidth());
+        // TODO: figure out center alignment
+
+        Label shortbuttontext = new Label("hi", Heidren.skin.optional("default", Label.LabelStyle.class));
+
+
+        // console add text
+        console.appendMessage("Hello, world! what happens if the text goes on for so goddamn long it has to wrap. does it wrap?? what next!!!?!", MessageType.GAME_EVENT); // Game Event Ex.
         console.appendMessage("This is NPC dialog.", MessageType.NPC_DIALOG); // NPC Event Ex.
 
         // test panel table
         // cannot reuse buttons, gotta make em fresh
-        textbutton = new TextButton("whats poppin", Heidren.skin); // test button 1
+        // test button 1
+        textbutton = new TextButton("", Heidren.skin);
         textbutton.setName("button1"); // declare button name to easily change the button text later
-        buttonpanel.add(textbutton).width(buttonpanel.getWidth()).fillY().space(5).uniform();
+        textbutton.add(shortbuttontext).width(buttonpanel.getWidth());
+        buttonpanel.add(textbutton).fill();
 
+        // button 2
         buttonpanel.row();
-        textbutton = new TextButton("whats poppin", Heidren.skin); // test button 2
+        textbutton = new TextButton("", Heidren.skin);
         textbutton.setName("button2"); // declare button name to easily change the button text later!!!
-        textbutton.setColor(Color.WHITE);
-        buttonpanel.add(textbutton).fill().space(5).uniform();
+        textbutton.add(longbuttontext).width(buttonpanel.getWidth());
+        textbutton.setColor(Color.WHITE); // doesn't do shit currently
+        buttonpanel.add(textbutton).fill().space(5);
 
+        // button 3
         buttonpanel.row();
-        textbutton = new TextButton("whats poppin", Heidren.skin);
+        textbutton = new TextButton("", Heidren.skin);
         textbutton.setName("button3"); // declare button name to easily change the button text later!!!
+        textbutton.add(shortbuttontext).width(100);
         buttonpanel.add(textbutton).fill().space(5).uniform();
 
+        // button 4
         buttonpanel.row();
-        textbutton = new TextButton("whats poppin", Heidren.skin);
-        textbutton.setName("button4"); // declare button name to easily change the button text later!!!
+        textbutton = new TextButton("", Heidren.skin);
+        textbutton.setName("button4"); // declare button name to (maybe) change button text later
         textbutton.add(space);
-        buttonpanel.add(textbutton).fill().space(5).uniform(); // test blank button
+        buttonpanel.add(textbutton).fill().space(5);
 
+        // change button 1
         textbutton = buttonStage.getRoot().findActor("button1");
-        textbutton.setText("what happens if its rally really long");
+        textbutton.reset();
+        textbutton.add("this text was changed and it is also very long").width(buttonpanel.getWidth());
+        //textbutton.add("lil change").width(buttonpanel.getWidth());
 
-
+        // root table construction
         roottable.row(); //r1 - title
-        roottable.add(title).expand(); // r2 c1
+        roottable.add(title); // r2 c1
 
+        //TODO: figure out images + add stats panel
         roottable.row(); //r2 - image window + stats panel
         roottable.add(space).grow().space(10);// r2 c1 image
         roottable.add(space); //r2 c2 stats
 
+        // TODO: sprite add + animation
         roottable.row(); // r3 - sprites (potentially 4 cols??)
         roottable.add(space);// r3 c1
         roottable.add(space); //r3 c2
         roottable.add(space);//r3 c3
 
-        roottable.row();
+        roottable.row(); // r4 - console + button panel
         roottable.add(console).grow().space(10); //r2c2
         roottable.add(buttonpanel).right().top().space(10);//r2c3
 
