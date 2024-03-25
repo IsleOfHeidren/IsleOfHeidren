2ab44b16224b23cfb71b504117c0db797afd8e08
Bella <67770613+bricklerb@users.noreply.github.com>
2024-03-25 16:02:45 -0400

    displaying map marker

--- src/core/src/com/github/isleofheidren/game/Map.java
+++ src/core/src/com/github/isleofheidren/game/Map.java
@@ -7,21 +7,38 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
+import java.io.File;
+
 public class Map extends Table {
     private short[][] grid;
     private int curCol;
     private int curRow;
 
+    private Pixmap mapImage;
     public Map() {
         super();
 
         FileHandle mapImage = Gdx.files.internal("game_map.png");
         Pixmap pixmap = new Pixmap(mapImage);
 
+        FileHandle xImage = Gdx.files.internal("current_location_marker.png");
+        Pixmap xMap = new Pixmap(xImage);
+
+        pixmap.drawPixmap(xMap, 252 - (xMap.getWidth() / 2), 189 - (xMap.getHeight() / 2));
 
         this.add(new Image(new Texture(pixmap)));
 
         initGrid();
+    }
+
+    private void moveXImage() {
+        int xStarting = 252;
+        int yStarting = 189;
+
+        int x = xStarting + (167 * curCol);
+        int y = yStarting + (167 * curRow);
+
+
     }
 
     private void initGrid() {
@@ -43,6 +60,43 @@ public class Map extends Table {
         this.curRow = row;
     }
 
+    public void goNorth() {
+        if (canGoNorth()) {
+            curRow--;
+        }
+    }
 
+    public void goSouth() {
+        if (canGoSouth()) {
+            curRow++;
+        }
+    }
 
+    public void goEast() {
+        if (canGoEast()) {
+            curCol++;
+        }
+    }
+
+    public void goWest() {
+        if (canGoWest()) {
+            curCol--;
+        }
+    }
+
+    public boolean canGoNorth() {
+        return curRow != 0;
+    }
+
+    public boolean canGoSouth() {
+        return curRow != 9;
+    }
+
+    public boolean canGoEast() {
+        return curCol != 9;
+    }
+
+    public boolean canGoWest() {
+        return curCol != 0;
+    }
 }
